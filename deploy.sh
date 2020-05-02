79098acfbc7282020806fcab294b6966cc8e4f66
dizak <dariusz.izak@ibb.waw.pl>
2020-05-02 14:05:10 +0200

    Change hard-coded deployment code to function with args

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #! /usr/bin/env bash
 
-gcloud functions deploy covinfo\
-    --region=europe-west3\
-    --trigger-http\
-    --runtime=python37\
-    --memory=128MB\
-    --source=./covinfo\
-    --entry-point=get_daily_data\
-    --allow-unauthenticated
+deploy(){
+# $1 name of the cloud function
+# $2 name of function from the main.py package to wrap as cloud function
+# $3 amount of RAM to assign to the cloud function
+    gcloud functions deploy "${1}"\
+	--region=europe-west3\
+	--trigger-http\
+	--runtime=python37\
+	--memory="${3}"\
+	--source=./covinfo\
+	--entry-point="${2}"\
+	--allow-unauthenticated
+}
